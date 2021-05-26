bc8bd3707bc5af58b0a7b2691c7276c3a24eb087
kianzzhang <kianzzhang@tencent.com>
2021-05-26 20:55:41 +0800

    -

---
+++ pages/Ruler/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import style from './index.module.css'
+
+class Ruler extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    <div className={style.background}>
+      
+    </div>
+  }
+
+  render() { 
+    return (
+        <div>
+        </div>
+    );
+  }
+}
+
+export default Ruler;
