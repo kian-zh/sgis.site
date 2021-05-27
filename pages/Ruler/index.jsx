244bbea980ad09a79ed2296aff40d20c9bfd6e60
kianzzhang <kianzzhang@tencent.com>
2021-05-27 20:46:11 +0800

    新增ruler页面

--- pages/Ruler/index.jsx
+++ pages/Ruler/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from './index.module.css'
+import style from './index.module.less'
 
 class Ruler extends React.Component {
   constructor() {
@@ -9,15 +9,19 @@ class Ruler extends React.Component {
   }
 
   componentDidMount() {
-    <div className={style.background}>
-      
-    </div>
+
   }
 
   render() { 
     return (
-        <div>
+      <div className={style.background}>
+        <div className={style.title}>
+          尺子<span className={style.subtitle}>真实的尺寸</span>
         </div>
+        <div className={style.ruler}>
+          
+        </div>
+      </div>
     );
   }
 }
